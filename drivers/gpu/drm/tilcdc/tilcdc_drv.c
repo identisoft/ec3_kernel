b549177076b5a38e34a2766fe924af817c70b6d2
Rashid Motala <rashidm@identisoft.net>
2019-10-23 14:21:43 +0200

    ec3_2019_10_23_4.14.150

--- drivers/gpu/drm/tilcdc/tilcdc_drv.c
+++ drivers/gpu/drm/tilcdc/tilcdc_drv.c
@@ -347,7 +347,7 @@ static int tilcdc_init(struct drm_driver *ddrv, struct device *dev)
 			priv->pixelformats = tilcdc_legacy_formats;
 			priv->num_pixelformats =
 				ARRAY_SIZE(tilcdc_legacy_formats);
-			bpp = 16; /* This is just a guess */
+			bpp = 24; /* This is just a guess */ /* Changed this from 16 to 24 bpp for the am335x EABR display legacy mode - RM 2017-02-02 */
 		}
 	}
 
